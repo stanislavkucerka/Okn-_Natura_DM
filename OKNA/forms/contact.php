67a4d17b337a4cc20d00b6f090b0ff7d20b802da
stanislavkucerka <stanislav.kucerka@gmail.com>
2023-05-18 23:14:11 +0200

    aktivovanie formulára

--- OKNA/forms/contact.php
+++ OKNA/forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'stanislav.kucerka@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,11 +24,11 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  /
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'stanislav.kucerka@gmail.com',
+    'password' => 'Chnochno1',
     'port' => '587'
   );
   */
